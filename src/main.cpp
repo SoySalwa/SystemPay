7b0b577d9712b33de0f69e7fd397c6e682878d01
SoySalwa <zoldyckk54@gmail.com>
2025-09-09 13:32:18 -0400

    fix: issue themes in Windows

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include <QApplication>
 #include "mainwindow.h"
+#include "ThemeManager.h"
 
 int main(int argc, char **argv)
 {
     QApplication a(argc, argv);
+    a.setStyle("Fusion");
+    a.setPalette(ThemeManager::instance()->allThemes()["Tema Oscuro"].palette);
     QApplication::setApplicationName("The System Pay");
     MainWindow w;
     w.resize(1280, 720);
